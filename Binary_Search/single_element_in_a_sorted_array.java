56e8da92dca815e50ed69b9a9d1f4072c9a4bc8c
saba akram <sabaakram211@gmail.com>
2023-12-02 13:02:14 +0530

    single element in a sorted array code modified

--- Binary_Search/single_element_in_a_sorted_array.java
+++ Binary_Search/single_element_in_a_sorted_array.java
@@ -1,6 +1,6 @@
 public class single_element_in_a_sorted_array {
     public static void main(String[] args) {
-        int arr[] = { 1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6 };
+        int arr[] = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6 };
         int n = arr.length;
         int min_element = findMin(arr, n);
         System.out.println("Mininum element in a sorted rotated array is : "+min_element);
@@ -9,6 +9,8 @@ public class single_element_in_a_sorted_array {
     public static int findMin(int[] arr, int n) {
         if (n == 1)
             return arr[0];
+        if(arr[0]!=arr[1])return arr[0];
+        if(arr[n-1]!=arr[n-2])return arr[n-1];
         int lo = 0, hi = n - 1;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
